f1076fbb89ccb90cc64e08d3c0c22ad0b7391d63
May <536440449>
2025-03-01 08:51:25 +0800

    解决请求头携带token错误

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,26 +1,46 @@
-import axios from "axios";
-import { Toast } from "vant";
-import { userRouter } from "vue-router"
-axios.defaults.baseURL = import.meta.env.MODE === 'development'
-  ? '//localhost:7001'
-  : '//47.99.134.126:7008';
-axios.defaults.withCredentials = true; 
-axios.defaults.headers["X-Requested-With"] = "XMLHttpRequest";
-axios.defaults.headers["token"] = localStorage.getItem("token") || "";
-axios.defaults.headers.post["Content-Type"] = "application/json";
-
-axios.interceptors.response.use((res)=>{
-    const router = userRouter()
-    if(typeof res.data !== "object"){
-        return Promise.reject(res);
-    }
-    if(res.data.code!==200){
-        if(res.data.msg) Toast.fail(res.data.msg);
-        if(res.data.code == 401){
-            router.push('/login')
-        }
-        return Promise.reject(res.data.data)
-    }
-    return res.data
+import axios from 'axios'
+import { useUserStore } from '@/stores/user'
+import router from '@/router'
+import { showFailToast } from 'vant'
+const instance = axios.create({
+  baseURL: 'http://192.168.10.5:7009/api/',
+  withCredentials: true,
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Type': 'application/json'
+  },
+  timeout: 5000
 })
-export default axios
+instance.interceptors.request.use(
+  function (config) {
+    const userStore = useUserStore()
+    if (userStore.token) {
+      // console.log('111', userStore.token)
+      // config.headers['token'] = userStore.token
+      // Authorization 是 HTTP 请求头中的一个标准字段，用于向服务器传递 ​客户端身份凭证。它是实现认证/授权机制的核心传输通道。
+      config.headers.Authorization = userStore.token
+    }
+    return config
+  },
+  function (error) {
+    console.log(error)
+    return Promise.reject(error)
+  }
+)
+instance.interceptors.response.use((res) => {
+  // console.log('111', res)
+  if (typeof res.data !== 'object') {
+    showFailToast('请求失败')
+    console.log(Promise.reject(res))
+    return Promise.reject(res)
+  }
+  if (res.data.code !== 200) {
+    if (res.data.msg) showFailToast(res.data.msg)
+    if (res.data.code == 401) {
+      router.push('/login')
+    }
+    return Promise.reject(res.data)
+  }
+  return res.data
+})
+export default instance
